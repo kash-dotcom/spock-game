ab9455e634eb215b60029d52af1d48260df7e59a
kash-dotcom <katyashworth27@gmail.com>
2024-09-18 21:46:56 +0000

    added an event listener to define userChoice

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -9,30 +9,51 @@ btn.onclick = function () {
 }
 
 
-//listening for the click
-let rock = document.getElementsByClassName("rock");
-
-function selected(event) {
-    console.log(event);
-    return "rock"
-};
-
-rock.addEventListener("click", selected);
 
 
 
 
 
 // userChoice
-
-
-let paper = document.getElementsByName("paper")
-let scissors = document.getElementsByName("scissors")
-let lizard = document.getElementsByName("lizard")
-let spock = document.getElementsByName("spock")
+let rock =  document.getElementById("rock");
+let paper = document.getElementById("paper")
+let scissors = document.getElementById("scissors")
+let lizard = document.getElementById("lizard")
+let spock = document.getElementById("spock")
 
 let userChoice;
 let computerChoice;
+
+//creating userChoice
+function rockSelected(event) {
+    userChoice = "rock";
+    console.log(userChoice);
+}
+function paperSelected(event) {
+    userChoice = "paper";
+    console.log(userChoice);
+}
+function scissorsSelected(event) {
+    userChoice = "scissors";
+    console.log(userChoice);
+}
+function lizardSelected(event) {
+    userChoice = "lizard";
+    console.log(userChoice);
+}
+function spockSelected(event) {
+    userChoice = "spock";
+    console.log(userChoice);
+}
+rock.addEventListener("click", rockSelected);
+paper.addEventListener("click", paperSelected);
+scissors.addEventListener("click", scissorsSelected);
+lizard.addEventListener("click", lizardSelected);
+spock.addEventListener("click", spockSelected);
+
+
+
+
 
 if (userChoice == "rock" && computerChoice == "scissors" || userChoice == "rock" && computerChoice == "lizard") {
     userWins = true;
