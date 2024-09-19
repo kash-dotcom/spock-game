336dedfd6956dc9aeb4355b527941383d0bbeab0
kash-dotcom <katyashworth27@gmail.com>
2024-09-19 19:52:55 +0000

    attempted to debug code as JS isn't working

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,68 +7,112 @@ var btn = document.getElementsByClassName("fa-circle-question")
 btn.onclick = function () {
     modal.style.display = "block";
 }
+/*
+// styling of ID on hover
+rock.style.backgroudColor = "black"; 
+*/
 
-
-// userChoice
+// userChoice - link to ID
 let rock =  document.getElementById("rock");
-let paper = document.getElementById("paper")
-let scissors = document.getElementById("scissors")
-let lizard = document.getElementById("lizard")
-let spock = document.getElementById("spock")
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let lizard = document.getElementById("lizard");
+let spock = document.getElementById("spock");
 
-let userChoice;
+//Computer is waiting for a click 
 
-//creating userChoice
-function rockSelected(event) {
-    userChoice = "rock";
-    console.log(userChoice);
-}
-function paperSelected(event) {
-    userChoice = "paper";
-    console.log(userChoice);
-}
-function scissorsSelected(event) {
-    userChoice = "scissors";
-    console.log(userChoice);
-}
-function lizardSelected(event) {
-    userChoice = "lizard";
-    console.log(userChoice);
-}
-function spockSelected(event) {
-    userChoice = "spock";
-    console.log(userChoice);
-}
 rock.addEventListener("click", rockSelected);
 paper.addEventListener("click", paperSelected);
 scissors.addEventListener("click", scissorsSelected);
 lizard.addEventListener("click", lizardSelected);
 spock.addEventListener("click", spockSelected);
 
+//creating userChoice
+var userChoice;
+function rockSelected() {
+    whosTheWinner();
+    console.log("user selected; " + userChoice);    
+    
+}
 
-//creating computerChoice - https://www.geeksforgeeks.org/how-to-select-a-random-element-from-array-in-javascript/
+function paperSelected() {
+    whosTheWinner();
+    console.log("user selected; " + userChoice);
+    
+   
+}
+function scissorsSelected() {
+    whosTheWinner();
+    console.log("user selected; " + userChoice);
+   
 
+    
+}
+function lizardSelected() {
+    whosTheWinner();
+    console.log("user selected; " + userChoice);
+  
+    
+}
+function spockSelected(event) {
+    whosTheWinner();
+    console.log("user selected; " + userChoice);
+   
+}
+
+//creating computerChoice - adpated from - https://www.geeksforgeeks.org/how-to-select-a-random-element-from-array-in-javascript/ - 
 
 let optionsArray = ["rock", "paper", "scissors", "lizard", "spock"];
 
 function computerChoice() {
-    console.log(optionsArray[(Math.floor(Math.random() * optionsArray.length))]);
+    let randomChoice = Math.floor(Math.random() * optionsArray.length);
+    return optionsArray[randomChoice];
 }
-computerChoice()
 
 //Who's the winner!! Computer Vs Human
 function whosTheWinner() {
-if (userChoice == "rock" && computerChoice == "scissors" || userChoice == "rock" && computerChoice == "lizard") {
-    userWins = true;
-    alert("")
-} else if (userChoice == "paper" && computerChoice == "rock"|| userChoice == "paper" && computerChoice == "spock") {
-    userWins = true;
-    console.log(userWins);
+    let computerChoiceValue = computerChoice();
+    let userWins = false;
+    console.log("computer selectect: "+ computerChoiceValue)
 
+    if ((userChoice === "rock" && computerChoiceValue === "scissors") || (userChoice === "rock" && computerChoiceValue === "lizard")) {
+        userWins = true;
+        alert("You win!");
+        console.log("user wins");
 
-} else (userChoice == "scissors" && computerChoice == "lizard" || userChoice == "scissors" && computerChoice == "paper"); {
-    userWins = true;
-    console.log(userWins);
+    } else if ((userChoice === "paper" && computerChoiceValue === "rock")|| (userChoice === "paper" && computerChoiceValue === "spock")) {
+        userWins = true;
+        alert("You win!");
+        console.log("user wins");
+
+    } else if ((userChoice === "scissors" && computerChoiceValue === "lizard") || (userChoice === "scissors" && computerChoiceValue === "paper")) {
+        userWins = true;
+        alert("You win!");
+        console.log("user wins");
+    } else {
+    alert("Computer wins!")
+    console.log("computer wins");
+    };
+return userWins;
 }
-}
-console.log(userWins);
+
+/*
+rock && computerChoice("scissors")
+"Rock crushes scissors"
+
+scissors && computerChoice("rock")
+"Rock crushes scissors"
+
+"Scissors cut Paper"
+
+"Paper covers Rock"
+"Rock crushes Lizard"
+"Lizard poisons Spock"
+"Spock smashes Scissors"
+"Scissors decapitates Lizard"
+"Lizard eats Paper"
+"Paper disproves Spock"
+"Spock vaporises Rock"
+"Rock crushes scissors"
+
+*/
